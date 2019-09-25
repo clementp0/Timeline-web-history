0432f4345ba0e1d42fd6d82a090ef2cdc116e7f1
Clement PO <clement.pommerie@gmail.com>
2019-09-25 14:29:17 +0200

    Added Progress Scroll bar

--- script.js
+++ script.js
@@ -54,3 +54,22 @@
 })(jQuery);
 
 $("#timeline-1").timeline();
+
+
+
+
+
+
+
+
+
+
+
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
