877cdea60655562e1ff95ac5e8a03077756198ec
Clement PO <clement.pommerie@gmail.com>
2019-10-01 12:45:30 +0200

    Service Worker for PWA
    Its here ! Its the Future

---
+++ sw.js
@@ -0,0 +1,38 @@
+﻿this.addEventListener("fetch", event => {
+    event.respondWith(
+        (async () => {
+            return await fetch(event.request);
+        })());
+});
+
+let cacheName = "history";
+
+this.addEventListener('install', function(event) {
+    event.waitUntil((async () => {
+        let cache = await caches.open(cacheName);
+        await cache.addAll([
+            'index.html'
+            'style.css',
+            'manifest.json',
+        ]);
+    })());
+});
+
+this.addEventListener("fetch", event => {
+    event.respondWith(
+        (async () => {
+            let cache = await caches.open(cacheName);
+            try {
+                await cache.add(event.request);
+            } catch (e) {
+                //Echec de l'ajout dans le cache (hors connection)
+            }
+            let response = await cache.match(event.request);
+            //Si la réponse n'est pas dans le cache, fallback sur une page d'information
+            if (!response) {
+                response = await cache.match("offline.html");
+            }
+            return response;
+        })()
+    );
+});
